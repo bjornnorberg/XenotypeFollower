8bbb0aa14538773172cd19a6c1b5c758b5738dd5
Bjorn Norberg <b.norbergs@gmail.com>
2024-01-08 07:56:39 +0100

    setup

---
+++ Source/XenotypeFollower/XenotypeFollower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace XenotypeFollower
+{
+    [StaticConstructorOnStartup]
+    public static class XenotypeFollower
+    {
+        static XenotypeFollower()
+        {
+            Log.Message("XenoType follower running!");
+        }
+    }
+}
