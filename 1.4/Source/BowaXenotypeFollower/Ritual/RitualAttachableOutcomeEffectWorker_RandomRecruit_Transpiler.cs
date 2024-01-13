27deec9b147641a43843974142f0a758ecdf0ade
Bjorn Norberg <b.norbergs@gmail.com>
2024-01-13 22:17:21 +0100

    use transpiler for better compatibility

---
+++ 1.4/Source/BowaXenotypeFollower/Ritual/RitualAttachableOutcomeEffectWorker_RandomRecruit_Transpiler.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Emit;
+using Verse;
+
+namespace BowaXenotypeFollower.Ritual
+{
+    [HarmonyPatch(typeof(RitualAttachableOutcomeEffectWorker_RandomRecruit), "Apply")]
+    public static class RitualAttachableOutcomeEffectWorker_RandomRecruit_Transpiler
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            ConstructorInfo targetConstructor = typeof(PawnGenerationRequest).GetConstructor(new[] { typeof(PawnKindDef), typeof(Faction), typeof(PawnGenerationContext), typeof(int), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(float), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(float), typeof(float), typeof(Pawn), typeof(float), typeof(Predicate<Pawn>), typeof(Predicate<Pawn>), typeof(IEnumerable<TraitDef>), typeof(IEnumerable<TraitDef>), typeof(float?), typeof(float?), typeof(float?), typeof(Gender?), typeof(string), typeof(string), typeof(RoyalTitleDef), typeof(Ideo), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(List<GeneDef>), typeof(List<GeneDef>), typeof(XenotypeDef), typeof(CustomXenotype), typeof(List<XenotypeDef>), typeof(float), typeof(DevelopmentalStage), typeof(Func<XenotypeDef, PawnKindDef>), typeof(FloatRange?), typeof(FloatRange?), typeof(bool) });
+
+            MethodInfo modifyMethod = AccessTools.Method(typeof(XenotypeSelector), nameof(XenotypeSelector.ModifyXenotypeInRequest));
+            var list = codes.ToList();
+
+            int index = list.FindIndex(code => code.opcode == OpCodes.Newobj && code.operand as ConstructorInfo == targetConstructor);
+
+            if (index != -1)
+            {
+                list.Insert(index + 1, new CodeInstruction(OpCodes.Call, modifyMethod));
+            }
+
+            return list.AsEnumerable();
+        }
+
+    }
+}
---
+++ 1.4/Source/BowaXenotypeFollower/Ritual/XenotypeSelector.cs
@@ -0,0 +1,55 @@
+﻿using BowaXenotypeFollower.Loader;
+using BowaXenotypeFollower.Settings;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BowaXenotypeFollower.Ritual
+{
+    public static class XenotypeSelector
+    {
+        public static PawnGenerationRequest ModifyXenotypeInRequest(PawnGenerationRequest request)
+        {
+
+            List<XenoPawn> xenoPawns = new List<XenoPawn>();
+
+            var allXenotypes = XenotypeFollowerSettings.BaseXenotypeDefNames.Select((name => new { Name = name, IsCustom = false })).Concat(
+                XenotypeFollowerSettings.CustomXenotypesDefNames.Select(name => new { Name = name, IsCustom = true }));
+
+            xenoPawns.AddRange(allXenotypes.Select(x => new XenoPawn
+            {
+                Name = x.Name,
+                isCustom = x.IsCustom
+            }));
+
+            if (xenoPawns.Count <= 0) return request;
+
+            XenoPawn rngXeno = xenoPawns.RandomElement();
+
+            IEnumerable<XenotypeDef> xenotypeDefs = DefDatabase<XenotypeDef>.AllDefs
+                    .Where((XenotypeDef x) => XenotypeFollowerSettings.BaseXenotypeDefNames.Contains(x.defName));
+            IEnumerable<CustomXenotype> customXenogerms = CustomLoader.CustomXenotypes.Where((CustomXenotype x) => XenotypeFollowerSettings.CustomXenotypesDefNames.Contains(x.name));
+
+            if (rngXeno.isCustom)
+            {
+                request.ForcedCustomXenotype = customXenogerms.FirstOrDefault((x) => x.name == rngXeno.Name);
+            }
+            else
+            {
+                request.ForcedXenotype = xenotypeDefs.FirstOrDefault((x) => x.defName == rngXeno.Name);
+            }
+            return request;
+
+        }
+
+        class XenoPawn
+        {
+            public string Name { get; set; }
+            public bool isCustom { get; set; }
+        }
+    }
+}
